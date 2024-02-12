f327ad554d45dd97f91c40b011cab80196feb90d
WHUthree <113110627+WHUthree@users.noreply.github.com>
2024-02-12 22:12:48 +0800

    使用同一模型实现recruit_message和meal_record的序列化

--- square/models.py
+++ square/models.py
@@ -1,16 +1,19 @@
 from django.db import models
-from django.core.validators import MinValueValidator,MaxValueValidator
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
 class recruit_message(models.Model):
-    #id = models.CharField(max_length=20, verbose_name=u"招募信息标识符", primary_key=True)
+    id = models.CharField(max_length=20, verbose_name=u"信息标识符", primary_key=True)
     title = models.CharField(max_length=20, verbose_name=u"标题")
     content = models.TextField(verbose_name="详情")
-    member_num = models.IntegerField(validators=[MinValueValidator(2,message="至少两人约饭")],verbose_name="约饭总人数")
+    member_num = models.IntegerField(validators=[MinValueValidator(2, message="至少两人约饭")],
+                                     verbose_name="约饭总人数")
     grade_choice = (
         (1, "大一"), (2, "大二"), (3, "大三"), (4, "大四"),
         (5, "研一"), (6, "研二"), (7, "研三"),
         (8, "博一"), (9, "博二"), (10, "博三"), (11, "博四")
     )
-    grade = models.SmallIntegerField(choices=grade_choice,verbose_name="年级")
+    grade = models.SmallIntegerField(choices=grade_choice, verbose_name="年级")
     location = models.CharField(max_length=30, verbose_name=u"地点")
 
     type_choice = (
@@ -18,7 +21,7 @@ class recruit_message(models.Model):
         (4, "西餐"), (5, "韩料"), (6, "日料"),
         (7, "甜品"), (8, "其它")
     )
-    type = models.SmallIntegerField(choices=type_choice,verbose_name="种类")
+    type = models.SmallIntegerField(choices=type_choice, verbose_name="种类")
     spicy_choice = (
         (1, "不辣"), (2, "微辣"), (3, "中辣"),
         (4, "重辣")
@@ -28,28 +31,23 @@ class recruit_message(models.Model):
     dinner_time = models.DateTimeField(verbose_name=u"用餐时间")
     image = models.ImageField(upload_to='images/', verbose_name=u"美团店铺截图")
     is_complete = models.BooleanField(default=False, verbose_name=u"是否完成")
+    likes = models.SmallIntegerField(default=0, verbose_name="点赞数")
     participants = models.ManyToManyField('user.user', related_name='recruit_messages')
+    post_user = models.ForeignKey('user.user', on_delete=models.CASCADE, verbose_name="发布者")
+
     class Meta:
         verbose_name = "招募信息"
         verbose_name_plural = verbose_name
-class meal_record(models.Model):
-    id = models.CharField(max_length=20, verbose_name=u"约饭记录标识符", primary_key=True)
-    title = models.CharField(max_length=20, verbose_name=u"标题")
-    dinner_time = models.DateTimeField(verbose_name=u"用餐时间")
-    location = models.CharField(max_length=100, verbose_name=u"地点")
-    image1 = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="图片1")
-    image2 = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="图片2")
-    image3 = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="图片3")
-    likes = models.SmallIntegerField(default=0, verbose_name="点赞数")
-    content = models.TextField(verbose_name=u"详情")
-    post_user = models.ForeignKey('user.user', on_delete=models.CASCADE, verbose_name="发布者")
 
     class Meta:
         verbose_name = "约饭记录"
         verbose_name_plural = verbose_name
+
+
 class Appraise(models.Model):
     recipient = models.ForeignKey('user.user', on_delete=models.CASCADE, verbose_name="被评价者")
     star = models.SmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name="星数")
+
     class Meta:
         verbose_name = "评价信息"
         verbose_name_plural = verbose_name
--- square/views.py
+++ square/views.py
@@ -1,5 +1,4 @@
 from .models import recruit_message
-from .models import meal_record
 from rest_framework.generics import GenericAPIView
 from rest_framework import serializers
 from rest_framework.response import Response
@@ -8,69 +7,90 @@ from rest_framework.response import Response
 class recruit_messageSerializers(serializers.ModelSerializer):
     class Meta:
         model = recruit_message
-        fields = "__all__"
+        exclude = {"likes"}
+
+
 class meal_recordSerializers(serializers.ModelSerializer):
     class Meta:
-        model = meal_record
-        fields = "__all__"
+        model = recruit_message
+        fields = {"id", "title", "dinner_time", "location", "image1", "image2", "image3", "likes", "content",
+                  "post_user"}
+
+
 class recruit_messageView(GenericAPIView):
     queryset = recruit_message.objects.all()
     serializer_class = recruit_messageSerializers
-    def get(self,request):
+
+    def get(self, request):
         serializer = self.get_serializer(instance=self.get_queryset(), many=True)
         return Response(serializer.data)
-    def post(self,request):
-        print("data",request.data)
-        serializer = self.get_serializer(data = request.data)
+
+    def post(self, request):
+        print("data", request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             recruit_message.objects.create(**serializer.validated_data)
         else:
             return Response(serializer.errors)
+
+
 class meal_recordView(GenericAPIView):
-    queryset = meal_record.objects.all()
+    queryset = recruit_message.objects.all()
     serializer_class = meal_recordSerializers
-    def get(self,request):
+
+    def get(self, request):
         serializer = self.get_serializer(instance=self.get_queryset(), many=True)
         return Response(serializer.data)
-    def post(self,request):
-        print("data",request.data)
-        serializer = self.get_serializer(data = request.data)
+
+    def post(self, request):
+        print("data", request.data)
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            meal_record.objects.create(**serializer.validated_data)
+            recruit_message.objects.create(**serializer.validated_data)
         else:
             return Response(serializer.errors)
+
+
 class recruit_messageDetailView(GenericAPIView):
     queryset = recruit_message.objects.all()
     serializer_class = recruit_messageSerializers
-    def get(self,request,pk):
+
+    def get(self, request, pk):
         serializer = self.get_serializer(instance=self.get_object(), many=False)
         return Response(serializer.data)
-    def put(self,request,pk):
+
+    def put(self, request, pk):
         print("data:", request.data)
-        serializer = self.get_serializer(instance=self.get_object(),data = request.data)
+        serializer = self.get_serializer(instance=self.get_object(), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-    def delete(self,request,pk):
+
+    def delete(self, request, pk):
         self.get_object().delete()
         return Response()
-#操作单一约饭记录
+
+
+# 操作单一约饭记录
 class meal_recordDetailView(GenericAPIView):
-    queryset = meal_record.objects.all()
+    queryset = recruit_message.objects.all()
     serializer_class = meal_recordSerializers
-    def get(self,request,pk):
+
+    def get(self, request, pk):
         serializer = self.get_serializer(instance=self.get_object(), many=False)
         return Response(serializer.data)
-    def put(self,request,pk):
+
+    def put(self, request, pk):
         print("data:", request.data)
-        serializer = self.get_serializer(instance=self.get_object(),data = request.data)
+        serializer = self.get_serializer(instance=self.get_object(), data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
-    def delete(self,request,pk):
+
+    def delete(self, request, pk):
         self.get_object().delete()
         return Response()
