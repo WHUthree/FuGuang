6070ca079615841f383b002f9b40977960bbe485
twomist <142470595+twomist@users.noreply.github.com>
2024-03-22 22:25:05 +0800

    Update urls.py in message

--- message/urls.py
+++ message/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('groupchat/<int:gid>/',views.show),
+    path('groupchat/<int:gid>/<int:uid>/',views.send),
     path('notifications/unread/<int:uid>/',views.unread),
     path('notifications/read/<int:uid>/',views.read)
 ]
